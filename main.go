b4ee145f53e8bb27a8f3e0e06fbacd1696bf23fb
shortdaddy0711 <sodoatm@gmail.com>
2021-02-03 11:06:51 -0800

    add readme file

--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	uploadedFile, header, err := r.FormFile("upload_file")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	defer uploadedFile.Close()
+
+	dirname := "./uploads"
+	os.MkdirAll(dirname, 0777)
+	filepath := fmt.Sprintf("%s/%s", dirname, header.Filename)
+	file, err := os.Create(filepath)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+	defer file.Close()
+	io.Copy(file, uploadedFile)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, filepath)
+
+}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/uploads", uploadHandler)
 
 	http.ListenAndServe(":3000", nil)
 }
